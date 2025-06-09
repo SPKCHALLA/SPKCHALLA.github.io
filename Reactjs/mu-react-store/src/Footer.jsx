c72614d586e77fa3b78ea0254afbb5a570cda73d
SPKCHALLA <shanmukhapadmakumar.challa@gmail.com>
2025-06-09 16:16:31 +0530

    updated p3

--- Reactjs/mu-react-store/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./Footer.css"
-export default function Footer() {
-  return (
-    <div className="App-Footer-Row">
-      $copy; 2025. All rights reserved.
-    </div>
-  )
-}
